d5cc3599e442ec4207c87e5fedc4150dd1a0b4c9
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2023-04-22 19:02:09 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  "https://namannangia-bug-free-system-xg5x7r4959g36ppx-8800.preview.app.github.dev/api";
+  "https://8800-namannangia-chatbotback-g6y6viorghw.ws-us95.gitpod.io/api";
